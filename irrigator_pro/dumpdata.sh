0916ed60017ec2db4c40accd8440b952f4564264
40365 <40365@7c370625-d93c-e311-9b11-002590954a86>
2014-02-05 21:16:45 +0000

    - Add more applciations to dumpdata script.

--- irrigator_pro/dumpdata.sh
+++ irrigator_pro/dumpdata.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 python manage.py dumpdata --indent 4 farms > farms/fixtures/initial_data.json
+python manage.py dumpdata --indent 4 admin emailuser > registration/fixtures/initial_data.json
+python manage.py dumpdata --indent 4 contact_info > contact_info/fixtures/initial_data.json
