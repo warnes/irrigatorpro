26239f619ef3f46112370c84408c5b46ef9e4856
40365 <40365@7c370625-d93c-e311-9b11-002590954a86>
2014-02-07 03:33:20 +0000

    Keep settings in local.template.py, use a softlink to local.py for devel system.

--- irrigator_pro/irrigator_pro/settings/local.template.py
+++ irrigator_pro/irrigator_pro/settings/local.template.py
@@ -11,18 +11,18 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 
-DATABASES = {
-    'default': {
-        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dev_database_name',
-        # The rest is not used with sqlite3:
-        'USER': 'dev_user',
-        'PASSWORD': 'dev_p@ssword',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         # The rest is not used with sqlite3:
+#         'USER': 'dev_user',
+#         'PASSWORD': 'dev_p@ssword',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 
 if DEBUG:
@@ -38,10 +38,6 @@ if DEBUG:
 
     INTERNAL_IPS = WildcardNetwork(['127.0.0.1', '192.168.*.*'])
 
-    # URL that handles the media, static, etc.
-    STATIC_URL = '/static/'
-    MEDIA_URL = STATIC_URL + 'media/'
-
     INSTALLED_APPS += (
         'debug_toolbar',
     )
@@ -51,21 +47,28 @@ if DEBUG:
     )
 
     # django-debug-toolbar specific
-    DEBUG_TOOLBAR_PANELS = (
-        'debug_toolbar.panels.version.VersionDebugPanel',
-        'debug_toolbar.panels.timer.TimerDebugPanel',
-        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-        'debug_toolbar.panels.headers.HeaderDebugPanel',
-        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-        'debug_toolbar.panels.template.TemplateDebugPanel',
-        'debug_toolbar.panels.sql.SQLDebugPanel',
-        'debug_toolbar.panels.signals.SignalDebugPanel',
-        'debug_toolbar.panels.logger.LoggingPanel',
-    )
+    DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+    ]
+
+    DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
     DEBUG_TOOLBAR_CONFIG = {
-        #'INTERCEPT_REDIRECTS': False,
+        'INTERCEPT_REDIRECTS': False,
         # 'SHOW_TOOLBAR_CALLBACK': custom_show_toolbar_function,
         #'EXTRA_SIGNALS': ['myproject.signals.MySignal'],
         'INSERT_BEFORE': '</body>',
     }
+
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
