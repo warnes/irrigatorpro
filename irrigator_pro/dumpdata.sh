63b487315e8eeed2a2ff25d70ee95f59b98f61e2
40365 <40365@7c370625-d93c-e311-9b11-002590954a86>
2014-02-17 21:52:14 +0000

    Split fixtures for admin and auth into separate directories. Add probe and probesync records.

--- irrigator_pro/dumpdata.sh
+++ irrigator_pro/dumpdata.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 python manage.py dumpdata --indent 4 farms > farms/fixtures/initial_data.json
-python manage.py dumpdata --indent 4 admin auth contact_info > contact_info/fixtures/initial_data.json
+python manage.py dumpdata --indent 4 contact_info > contact_info/fixtures/initial_data.json
+python manage.py dumpdata --indent 4 admin > admin/fixtures/initial_data.json
+python manage.py dumpdata --indent 4 auth > auth/fixtures/initial_data.json
