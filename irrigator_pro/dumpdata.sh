9f9571ba091d254dc28f83fe884aaba7eb3c648a
40365 <40365@7c370625-d93c-e311-9b11-002590954a86>
2014-02-17 19:32:18 +0000

    Make sure and export authentication information!

--- irrigator_pro/dumpdata.sh
+++ irrigator_pro/dumpdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python manage.py dumpdata --indent 4 farms > farms/fixtures/initial_data.json
-python manage.py dumpdata --indent 4 admin emailuser contact_info > contact_info/fixtures/initial_data.json
+python manage.py dumpdata --indent 4 admin auth contact_info > contact_info/fixtures/initial_data.json
