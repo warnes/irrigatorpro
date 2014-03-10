b7720261c4d6592ed78c91c384d50b0210888e11
40365 <40365@7c370625-d93c-e311-9b11-002590954a86>
2014-03-10 18:31:59 +0000

    Further work on water_register table

--- irrigator_pro/R/water_register.py
+++ irrigator_pro/R/water_register.py
@@ -43,31 +43,31 @@ def calculateAWC_ProbeReading(field, date):
     radio_id = probe.radio_id
 
     ## Determine the crop that corresponds to this date
-    #try:
-    if 1:
-        print "Query...",
-        crop_season = CropSeason.objects.filter(field_list=field,
-                                                season_start_date__gte=date).order_by("-season_start_date")#.first()
-        print "crop_season=", crop_season
-        #!FIXME: So season-end date is available, so this doesn't
-        #!prevent incorrect matching against a previous crop season if
-        #!this filed is not included in a later one.
-        print "Query...",
-        crop = crop_season.crop
-        print "crop=", crop
-        max_root_depth = CropSeason.crop.max_root_depth
-        print "max_root_depth=", max_root_depth
-    #except:
-    #    return None
+    crop_season = CropSeason.objects.filter(field_list=field,
+                                            season_start_date__lte=date,
+                                            season_end_date__gte=date,
+                                            )
+    if( crop_season.count() > 1 ):
+        raise ImproperlyConfigured("More than one crop season for field '%s' on %s." % (field, date))
+    crop_season = crop_season.first()
 
+    ## Get the maximum root depth
+    if crop_season:
+        crop = crop_season.crop
+        max_root_depth = crop_season.crop.max_root_depth
+    else:
+        return None
 
     ## Find any probe readings for date with this radio_id
     probe_reading = ProbeReading.objects.filter(radio_id=radio_id,
-                                                reading_datetime__startswith=date).order_by('reading_datetime').last()
+                                                reading_datetime__startswith=date)
 
+    ## Get the most recent one for this date
+    probe_reading = probe_reading.order_by('reading_datetime').last()
     if not probe_reading:
         return None
 
+    ## Extract soil parameters
     soil_type_parameters = SoilTypeParameter.objects.filter(soil_type=field.soil_type)
 
     try:
@@ -85,32 +85,31 @@ def calculateAWC_ProbeReading(field, date):
     except ObjectDoesNotExist:
         raise ImproperlyConfigured("Missing parameters for soiltype '%s': %d inch depth missing" % ( field.soil_type, 24) )
 
-
-    # IF(O48="","",IF(O48=0,$H$5,IF(((R$3+R$4*LN(O48))-(R$3+R$4*LN(40)))*24>$H$5,$H$5,((R$3+R$4*LN(O48))-(R$3+R$4*LN(40)))*24)))
-    #
-    # O48  = probe_reading.soil_potential_8
-    # H$5$ = field.soil_type.max_available_water
-    # R$3  = soil_type_8in.intercept
-    # R$4  = soil_type_8in.slope
-    #
-    # IF(O48="","",
-    #    IF(O48=0,
-    #       $H$5,
-    #       IF(
-    #               (
-    #                   (R$3+R$4*LN(O48))-(R$3+R$4*LN(40))
-    #               )*24>$H$5,
-    #               $H$5,
-    #               (
-    #                   (R$3+R$4*LN(O48))-(R$3+R$4*LN(40))
-    #               )*24
-    #       )
-    #   )
-    # )
-
-    # =IF(O48="","",IF(O48=0,$H$5,IF(((R$3+R$4*LN(O48))-(R$3+R$4*LN(40)))*24>$H$5,$H$5,((R$3+R$4*LN(O48))-(R$3+R$4*LN(40)))*24)))
-    # =IF(P48="","",IF(P48=0,$H$5,IF(((S$3+S$4*LN(P48))-(S$3+S$4*LN(40)))*24>$H$5,$H$5,((S$3+S$4*LN(P48))-(S$3+S$4*LN(40)))*24)))
-    # =IF(Q48="","",IF(Q48=0,$H$5,IF(((T$3+T$4*LN(Q48))-(T$3+T$4*LN(40)))*24>$H$5,$H$5,((T$3+T$4*LN(Q48))-(T$3+T$4*LN(40)))*24)))
+    #####
+    ## Calculate available water content (AWC) at each depth
+    #####
+    ##
+    ## The AWC is the difference between the total water in a block of
+    ## soil 24" tall at the sensor reading and the total water in that
+    ## same block of soil at a sensor reading of 40 kPa.  40 kPa is
+    ## considered to be the "threshold" value at which the plant can
+    ## no longer extract water efficiently.
+    ##
+    ## The total water is calculated at the sensor reading:
+    ##    TWC(probe) = [b0 + b1*ln(probe)]*24
+    ##
+    ## The total water at our threshold value is also calculated:
+    ##    TWC(40) = [b0 + b1*ln(40)]*24
+    ##
+    ## The difference between the two is the Available Water Content:
+    ##    AWC      = TWC(probe) - TWC(40)
+    ##             = {[b0 + b1*ln(probe)]*24 - [b0 + b1*ln(40)]*24}
+    ##             = 24*{[b0 +b1*ln(probe)] - [b0 + b1*ln(40)]}
+    ##             = 24 * {b0 - b0 + b1*ln(probe) - b1*ln(40)}
+    ##             = 24 *{b1*ln(probe) - b1*ln(40)}
+    ##             = 24 * b1 * [ln(probe) - ln(40)]
+    ##
+    ##
 
     def safelog( val ):
         if val <= 0:
@@ -132,47 +131,62 @@ def calculateAWC_ProbeReading(field, date):
     #print 'AWC 16"=%4.2f' % AWC_16
     #print 'AWC 24"=%4.2f' % AWC_24
 
-
-    AWC = (AWC_8 + AWC_16 + AWC_24)/3
-
-    #print 'Mean AWC=%4.2f' % AWC
-
-    if AWC > field.soil_type.max_available_water:
-        AWC = field.soil_type.max_available_water
+    #####
+    ## Calculate average AWC at the depths accessible to the crop
+    ## roots.
+    #####
+    ## NB: This would probably be better done via a linear
+    ##     interpolation, rather than discrete steps.
+    if max_root_depth <= 8:
+        AWC = AWC_8
+    elif max_root_depth <= 16:
+        AWC = (AWC_8 + AWC_16) / 2
+    else: # max_root_depth > 16
+        AWC = (AWC_8 + AWC_16 + AWC_24)/3
 
     return AWC
 
 
+def caclulateAWC_RainIrrigation(field, date):
+
+    wh = WaterHistory.objects.filter(field_list=field,
+                                     date=date
+                                    ).last()
+
+    if wh:
+        return ( wh.rain, wh.irrigation )
+    else:
+        return ( 0.0, 0.0 )
+
+
+
 if __name__ == "__main__":
     farm = Farm.objects.get(pk=1)
-    field = Field.objects.filter(farm=farm)[0]
+    field = Field.objects.filter(farm=farm).first()
 
+    AWC_initial = 2.5
+
+    print "** Farm         :", farm
+    print "** Field        :", field
+    print "** Starting AWC :", AWC_initial
+    print
+
+    AWC_prev = AWC_initial
     for day_of_month in range(1,31):
         date = datetime.date(2013, 4, day_of_month)
-        print "Calculating AWC for date: %s" % date,
+        print "AWC for date: %s  " % date,
         AWC = calculateAWC_ProbeReading(field, date)
-        print "=", AWC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if AWC:
+            AWC_plus = float("NaN")
+            print "  (probes)  = %3.2f" % ( AWC )
+        else:
+            AWC_plus = caclulateAWC_RainIrrigation(field, date)
+            AWC = AWC_prev + float(AWC_plus[0]) + float(AWC_plus[1])
+            print "%3.2f + %3.2f + %3.2f = %3.2f" % ( AWC_prev,
+                                                      AWC_plus[0],
+                                                      AWC_plus[1],
+                                                      AWC )
+        AWC_prev = AWC
 
 
 
