094954c52c92f4b5ebbea4c5ffa3cac8f5de78b9
Gregory R. Warnes <greg@warnes.net>
2015-02-24 21:43:17 -0600

    Remove unnecessary text from home page template

--- irrigator_pro/home/views.py
+++ irrigator_pro/home/views.py
@@ -4,8 +4,8 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     template_name = 'home.html'
 
-    def get(self, request, *args, **kwargs):
-        context = {
-            'some_dynamic_value': 'Current Status: Validation',
-        }
-        return self.render_to_response(context)
+#    def get(self, request, *args, **kwargs):
+#        context = {
+#            'some_dynamic_value': 'Current Status: Validation',
+#        }
+#        return self.render_to_response(context)
