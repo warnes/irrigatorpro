701a3e48ffc4038634d8933af666c636977f63e0
Alain Leblanc <aalebl@gmail.com>
2015-03-13 11:15:14 -0400

    Don't dump roles.

--- irrigator_pro/dumpdata.sh
+++ irrigator_pro/dumpdata.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-python manage.py dumpdata --indent 4 farms > farms/fixtures/initial_data.json
+python manage.py dumpdata --indent 4  farms > farms/fixtures/initial_data.json
+
+python manage.py dumpdata --indent 4  notifications > notifications/fixtures/initial_data.json
+
 python manage.py dumpdata  --indent 4 contact_info > contact_info/fixtures/initial_data.json
 python manage.py dumpdata --exclude contenttypes --indent 4 \
     account \
