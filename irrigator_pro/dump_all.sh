9d586518e9f6126dddb635eb33033b2276e6782c
Gregory R. Warnes <greg@warnes.net>
2015-03-13 08:44:54 -0500

    Provide a script to write all database content to a json file

---
+++ irrigator_pro/dump_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+FILENAME=dumpdata_`date --rfc-3339=seconds | sed -e 's/ /_/g'`.json.bz2
+
+echo "Dumping database contents to ${FILENAME}..."
+python manage.py dumpdata \
+    contact_info \
+    farms        \
+    notifications \
+    --exclude contenttypes \
+    account \
+    auth.User \
+    auth.Group \
+    sites \
+    | bzip2 > $FILENAME
+echo "Done."
