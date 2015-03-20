f0a20a3296344ca3946daf6bb77f6cdd3bcadb85
Alain Leblanc <aalebl@gmail.com>
2015-03-20 15:29:01 -0400

    Added signal handler to remove recipients of a notification rule when the recipients no longer listed as user on farm.

--- irrigator_pro/notifications/models.py
+++ irrigator_pro/notifications/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from common.models import Audit, Comment
+from django.db.models import Q
 
-from farms.models import CropSeason, Field
-
+from farms.models import CropSeason, Field, Farm
 
 
 ########################################################
@@ -33,3 +35,20 @@ class NotificationsRule(Comment, Audit):
     # Will use format "hh:mm" to save the time
     delivery_time       = models.CharField(max_length = 10)
     time_zone           = models.CharField(max_length = 50)
+
+
+    @receiver(post_save, sender=Farm)
+    def update_recipients(sender, **kwargs):
+        # Must be an easier way, but this will work for now
+        farm = kwargs.get('instance', None)
+        farm_users = farm.get_farmer_and_user_objects()
+        for notify in NotificationsRule.objects.all():  # Here should filter by checking farm in first field. .filter(Q(field_list__[0].farm = sender)):
+            aField = notify.field_list.all()[0]
+            if aField.farm != farm: continue
+            for recipient in notify.recipients.all():
+                if recipient not in farm_users:
+                    notify.recipients.remove(recipient)
+            notify.save()
+
+    
+            
