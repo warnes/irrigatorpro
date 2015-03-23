56d0638548c2f1ab0c0d77408cc21e196d311944
Alain Leblanc <aalebl@gmail.com>
2015-03-23 12:09:34 -0400

    Minor change to form handling to allow for authorized users to be sorted in the Farm form.

--- irrigator_pro/farms/forms.py
+++ irrigator_pro/farms/forms.py
@@ -3,10 +3,35 @@ from django.forms.models import inlineformset_factory, modelformset_factory
 from django.forms import Textarea
 from farms.models import Farm, Field, CropSeason, CropSeasonEvent, Probe
 from common.models import Audit, Comment, Location, NameDesc
+from django.contrib.auth.models import User
+
 
 class FarmForm(ModelForm):
     class Meta:
         model = Farm
+        fields = ['farmer',
+                  'name',
+                  'description',
+                  'users',
+                  'address_1',
+                  'address_2',
+                  'city',
+                  'county',
+                  'state',
+                  'zipcode',
+                  'gps_latitude', 
+                  'gps_longitude'
+              ]
+
+
+        # From: http://stackoverflow.com/questions/6541477/ordering-choices-in-modelform-manytomanyfield-django
+        # Doesn't look like it's used at all
+    def __init__(self, *args, **kwargs):
+        super(FarmForm, self).__init__(*args, **kwargs)   
+
+
+        self.fields['users'].queryset = User.objects.all().order_by('last_name')
+        
 
 FieldFormSet = inlineformset_factory(Farm, 
                                      Field, 
