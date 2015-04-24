033ba353bf94e15044635fe20c36b2ef3001e7cb
Gregory R. Warnes <greg@warnes.net>
2015-04-24 12:58:53 -0500

    - Add AWC to daily water plot - Modify cumulative water plot to use dashed line for future predictions - Change color selection in plots to:  DWU=green, Rainfall=cyan, Irrigation=blue, AWC=red, TotalWater=magenta

--- irrigator_pro/farms/water_register_plots.py
+++ irrigator_pro/farms/water_register_plots.py
@@ -59,10 +59,12 @@ def plot_daily_use(request, crop_season, field):
     wu_plot_before=fig.add_subplot(111)
     ir_plot_before=fig.add_subplot(111)
     rf_plot_before=fig.add_subplot(111)
+    aw_plot_before=fig.add_subplot(111)
 
     wu_plot_after=fig.add_subplot(111)
     ir_plot_after=fig.add_subplot(111)
     rf_plot_after=fig.add_subplot(111)
+    aw_plot_after=fig.add_subplot(111)
     
     x=[]
     x_pre = []
@@ -71,33 +73,39 @@ def plot_daily_use(request, crop_season, field):
     wu_pre=[]
     rf_pre=[]
     ir_pre=[]
+    aw_pre=[]
 
     wu_post=[]
     rf_post=[]
     ir_post=[]
+    aw_post=[]
     for wr in wr_list:
         x.append(wr.date)
         if (wr.date <= report_date):
             wu_pre.append(wr.daily_water_use)
             rf_pre.append(wr.rain)
             ir_pre.append(wr.irrigation)
+            aw_pre.append(wr.average_water_content)
             x_pre.append(wr.date)
         if (wr.date >= report_date):
             wu_post.append(wr.daily_water_use)
             rf_post.append(wr.rain)
             ir_post.append(wr.irrigation)
+            aw_post.append(wr.average_water_content)
             x_post.append(wr.date)
 
 
-    wu_plot_before.plot_date(x_pre, wu_pre, 'b-', label = "Water Usage")
-    rf_plot_before.plot_date(x_pre, rf_pre, 'g-', label = "Rain")
-    ir_plot_before.plot_date(x_pre, ir_pre, 'r-', label = "Irrigation")
+    wu_plot_before.plot_date(x_pre, wu_pre, 'g-', label = "Water Usage")
+    rf_plot_before.plot_date(x_pre, rf_pre, 'c-', label = "Rain")
+    ir_plot_before.plot_date(x_pre, ir_pre, 'b-', label = "Irrigation")
+    aw_plot_before.plot_date(x_pre, aw_pre, 'r-', label = "Average Water Content")
 
-    wu_plot_after.plot_date(x_post, wu_post, 'b:')
-    rf_plot_after.plot_date(x_post, rf_post, 'g-')
-    ir_plot_after.plot_date(x_post, ir_post, 'r-')
+    wu_plot_after.plot_date(x_post, wu_post, 'g:')
+    rf_plot_after.plot_date(x_post, rf_post, 'c:')
+    ir_plot_after.plot_date(x_post, ir_post, 'b:')
+    aw_plot_after.plot_date(x_post, aw_post, 'r:')
 
-    wu_plot_before.legend(loc = 'upper left')
+    wu_plot_before.legend(loc = 'top right')
     wu_plot_before.set_ylabel("Inches", fontsize=16)
 
     wu_plot_before.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
@@ -107,7 +115,6 @@ def plot_daily_use(request, crop_season, field):
     fig.suptitle("Daily Water Use", fontsize = 20)
     canvas=FigureCanvas(fig)
     response=django.http.HttpResponse(content_type='image/png')
-    #canvas.print_png(response)
     fig.savefig(response, transparent=True, format='png')
 
     return response
@@ -120,53 +127,71 @@ def plot_cumulative_use(request, crop_season, field):
 
     fig=Figure()
 
-    wu_plot=fig.add_subplot(111)
-    ir_plot=fig.add_subplot(111)
-    rf_plot=fig.add_subplot(111)
-    tw_plot=fig.add_subplot(111) # tw=total water = ir+rf
+    wu_before_plot=fig.add_subplot(111)
+    ir_before_plot=fig.add_subplot(111)
+    rf_before_plot=fig.add_subplot(111)
+    tw_before_plot=fig.add_subplot(111) # tw=total water = ir+rf
 
+    wu_after_plot=fig.add_subplot(111)
+    ir_after_plot=fig.add_subplot(111)
+    rf_after_plot=fig.add_subplot(111)
+    tw_after_plot=fig.add_subplot(111) # tw=total water = ir+rf
 
-    x=[]
+    x_before=[]
+    wu_before=[]
+    rf_before=[]
+    ir_before=[]
+    tw_before=[]
 
-    wu=[]
-    rf=[]
-    ir=[]
-    tw=[]
-
+    x_after=[]
+    wu_after=[]
+    rf_after=[]
+    ir_after=[]
+    tw_after=[]
 
     wu_total = 0
     ir_total = 0
     rf_total = 0
     tw_total = 0
     for wr in wr_list:
-        x.append(wr.date)
         wu_total += wr.daily_water_use
-        wu.append(wu_total)
-
         rf_total += wr.rain
-        rf.append(rf_total)
-
         ir_total += wr.irrigation
-        ir.append(ir_total)
-
 	tw_total += wr.rain + wr.irrigation
-        tw.append(tw_total)
 
-    wu_plot.plot_date(x, wu, 'b-', label = "Water Usage")
-    rf_plot.plot_date(x, rf, 'g-', label = "Rain")
-    ir_plot.plot_date(x, ir, 'r-', label = "Irrigation")
-    tw_plot.plot_date(x, tw, 'm-', label = "Rain + Irrigation")
-    wu_plot.legend(loc = 'upper left')
+        if (wr.date <= report_date):
+            x_before.append(wr.date)
+            wu_before.append(wu_total)
+            rf_before.append(rf_total)
+            ir_before.append(ir_total)
+            tw_before.append(tw_total)
+        else:
+            x_after.append(wr.date)
+            wu_after.append(wu_total)
+            rf_after.append(rf_total)
+            ir_after.append(ir_total)
+            tw_after.append(tw_total)
 
-    wu_plot.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
-    wu_plot.set_ylabel("Inches", fontsize=16)
+
+    wu_before_plot.plot_date(x_before, wu_before, 'g-', label = "Water Usage")
+    rf_before_plot.plot_date(x_before, rf_before, 'c-', label = "Rain")
+    ir_before_plot.plot_date(x_before, ir_before, 'b-', label = "Irrigation")
+    tw_before_plot.plot_date(x_before, tw_before, 'm-', label = "Rain + Irrigation")
+
+    wu_after_plot.plot_date(x_after, wu_after, 'g:')
+    rf_after_plot.plot_date(x_after, rf_after, 'c:')
+    ir_after_plot.plot_date(x_after, ir_after, 'b:')
+    tw_after_plot.plot_date(x_after, tw_after, 'm:')
+
+    wu_before_plot.legend(loc = 'upper left')
+
+    wu_before_plot.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+    wu_before_plot.set_ylabel("Inches", fontsize=16)
 
     fig.autofmt_xdate()
     canvas=FigureCanvas(fig)
     fig.suptitle("Cumulative Water Use", fontsize = 20)
     response=django.http.HttpResponse(content_type='image/png')
-#    canvas.print_png(response)
-#    fig.savefig(response, transparent=True, format='png')
     fig.savefig(response, transparent=True, format='png')
 
     return response
