173a3c931b9ed96e5636772ac255a4a7d104a269
Gregory R. Warnes <greg@warnes.net>
2015-05-19 23:03:19 -0500

    Complete work to get email autocomplete working for new farm form.

--- irrigator_pro/irrigator_pro/urls.py
+++ irrigator_pro/irrigator_pro/urls.py
@@ -116,9 +116,8 @@ urlpatterns = patterns('',
 
 
                        ## Ajax pages
-                       url(r'^farm/filter_auth/(?P<farm_pk>\d+)/',    'contact_info.views.get_available_users', name='filter_auth_users'),
-                       ### Need one for a new farm
-                       url(r'^farm/filter_auth/(?P<farm_pk>.*)/',   'contact_info.views.get_available_users', {'farm_pk': None}, name='filter_auth_users'),
+                       url(r'^filter_auth/$',                    'contact_info.views.get_available_users', name='filter_auth_users'),
+                       url(r'^filter_auth/(?P<farm_pk>\d+)/',    'contact_info.views.get_available_users', name='filter_auth_users_farm'),
 )
 
 if settings.DEBUG:
