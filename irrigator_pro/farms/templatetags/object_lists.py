9fc7c1a16dc3d9cd64f2af28aa621851b46593b6
Gregory R. Warnes <greg@warnes.net>
2015-08-04 19:05:20 -0500

    Optimize queries for rendering navbar-side down from 117 to 48.

--- irrigator_pro/farms/templatetags/object_lists.py
+++ irrigator_pro/farms/templatetags/object_lists.py
@@ -20,9 +20,11 @@ def farm_list(parser, token):
     Return a list of all Farm objects corresponding to request.user
     """
     def farm_list_wrap(context):
+        if 'farm_list' in context: return ''  # use existing result if present
+
         user = context['request'].user
         farm_list = Farm.objects.filter( Q(farmer=user) |
-                                         Q(users=user) ).distinct()
+                                         Q(users=user) ).distinct().prefetch_related('field_set')
         context['farm_list'] = farm_list
 
         for farm in farm_list:
@@ -39,31 +41,31 @@ def crop_season_list(parser, token):
     Return a list of all CropSeason objects corresponding to request.user
     """
     def crop_season_list_wrap(context):
+        if 'crop_season_list' in context: return ''  # use existing result if present
+
         user = context['request'].user
         crop_season_list = CropSeason.objects.filter( Q(field_list__farm__farmer=user) |
-                                                      Q(field_list__farm__users=user) ).distinct()
-
-        # sort list by descending year so recent seasons are at the top
-        crop_season_list = sorted(crop_season_list, 
-                                  key = lambda cs: -cs.season_start_date.year )
-
-        def inner(cs):
-          print cs.season_start_date
-
-        map(inner, crop_season_list )
-
+                                                      Q(field_list__farm__users=user) ).\
+                                  distinct().order_by('name'). \
+                                  prefetch_related('field_list', 
+                                                   'field_list__farm',
+                                                   'probe_set', 
+                                                   #'probe_set__field'
+                                                   )
         context['crop_season_list'] = crop_season_list
         
         for crop_season in crop_season_list:
-            crop_season.year = crop_season.season_start_date.year
+            crop_season.year           = crop_season.season_start_date.year
             crop_season.field_list_all = crop_season.field_list.all()
-            crop_season.probe_list_all = crop_season.probe_set.all()
+            crop_season.probe_list_all = crop_season.probe_set.all() \
+                                                     .select_related('field', 'farm')  \
+                                                     .distinct() 
+
             probe_field_list = []
-            for probe in crop_season.probe_set.all():
+            for probe in crop_season.probe_list_all:
               probe_field_list.append( (probe.field, probe) )
 
-
-            probe_field_list.sort(key=lambda i:str(i[0]) )
+            #probe_field_list.sort(key=lambda i:str(i[0]) )
             crop_season.probe_field_list = probe_field_list
 
         return ''
